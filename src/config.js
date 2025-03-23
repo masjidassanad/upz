2a13a95b051d5a6d52a707b71e8eb365dea2537d
Mudzakkir <707089+qaharmdz@users.noreply.github.com>
2025-03-23 21:56:03 +0700

    Deploy test

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 const config = {
   app: {
     name: 'UPZ Masjid Assanad',
-    version: '1.0.2',
+    version: '1.0.2-dev.1',
     author: 'UPZ Masjid Assanad',
     url: 'https://masjidassanad.github.io',
   },
